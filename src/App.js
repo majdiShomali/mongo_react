289bc4213bd18ca309513c9d0bbe1e3dac285e20
majdiShomali <majdi.shomali.1997@gmail.com>
2023-06-15 10:19:00 +0300

    edit the files

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import NavListMenu from "./components/Navbar";
 // import Home from "./pages/Home";
-// import Contact from "./pages/Contact";
+import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 // import Kitchen from "./pages/Kitchen";
 // import Admin from "./pages/Admin";
-// import About from "./pages/aboutPage/About";
+import About from "./pages/aboutPage/About";
 // import Recipes from "./pages/Recipes";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/Login";
@@ -88,8 +88,8 @@ export default function App() {
         <NavListMenu />
         <Routes>
           {/* <Route index element={<Home />} /> */}
-          {/* <Route path="ContactUs" element={<Contact />} />
-          <Route path="About" element={<About />} /> */}
+          <Route path="ContactUs" element={<Contact />} /> 
+          <Route path="About" element={<About />} /> 
           <Route path="SignUp" element={<SignUp />} />
           <Route path="LogIn" element={<LogIn />} />
           {/* <Route path="ShowRecipe" element={<ShowRecipe />} />
